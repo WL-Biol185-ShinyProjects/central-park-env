e4797d09b38855aea829ae3e7f3cddf36bb941e4
Cindy <cxie@mail.wlu.edu>
2026-03-04 16:44:09 -0500

    made it actually a dashboard

--- ui.R
+++ ui.R
@@ -1,8 +1,13 @@
 library(shinydashboard)
 
-dashboardPage((skin= "#652A0E"),
+dashboardPage(
   dashboardHeader(title = "Squirrel Exploration"),
-  dashboardSidebar(title = "What's the squirrel doing?"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
+      menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+    )
+  ),
   dashboardBody(
     fluidRow(
       box(plotOutput("squirrel_plot", height = 250)), box(plotOutput("squirrel_plot2", height = 250)
