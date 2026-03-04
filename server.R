60f02b97fff6d26e37ddb8e1b9b22fb2ceefa352
Cindy <cxie@mail.wlu.edu>
2026-03-04 16:02:38 -0500

    creating sidebar

--- server.R
+++ server.R
@@ -1,3 +1,19 @@
 library(shiny)
 library(ggplot2)
 library(tidyverse)
+
+function(input, output) {
+  
+  output$central_park_attitude <- renderPlot({
+    
+    flights %>% 
+      filter(
+        origin == input$airport,
+        dep_time == input$time
+      ) %>%
+      
+      ggplot(aes(dep_delay)) + geom_density()
+    
+  })
+  
+}
