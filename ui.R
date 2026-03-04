2847ff00be369349be76a18066c3badc31c37e5d
jmoore <jmoorel@mail.wlu.edu>
2026-03-04 16:48:29 -0500

    commit to pull

--- ui.R
+++ ui.R
@@ -1,6 +1,6 @@
 library(shinydashboard)
 
-dashboardPage((skin= "#652A0E"),
+dashboardPage((skin = "#652A0E"),
   dashboardHeader(title = "Squirrel Exploration"),
   dashboardSidebar(title = "What's the squirrel doing?"),
   dashboardBody(
