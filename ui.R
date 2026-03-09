bfae547a60d2050bc4f7b9939b5d64861d175cd0
Cindy <cxie@mail.wlu.edu>
2026-03-09 16:01:39 -0400

    switched over to the right tab

--- ui.R
+++ ui.R
@@ -8,16 +8,23 @@ dashboardPage(skin = "green",
   dashboardSidebar(title = "Squirrels in Central Park",
     sidebarMenu(
       menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-      menuItem("Meet the Squirrels", tabName = "Meet the Squirrels", icon = icon("tree")),
-      menuItem("Where Are They", tabName = "Where Are They", icon = icon("map")),
-      menuItem("What are They Doing", tabName = "What are They Doing", icon = icon("walking")),
-      menuItem("Park Conditions", tabName = "Park Conditions", icon = icon("sun"))
+      menuItem("Meet the Squirrels", tabName = "meet_the_squirrels", icon = icon("tree")),
+      menuItem("Where Are They", tabName = "where_are_they", icon = icon("map")),
+      menuItem("What are They Doing", tabName = "what_are_they_doing", icon = icon("walking")),
+      menuItem("Park Conditions", tabName = "park_conditions", icon = icon("sun"))
     )
   ),
   dashboardBody(
-    fluidRow(
-      box(plotOutput("squirrel_plot4", height = 250)), box(plotOutput("squirrel_plot2", height = 250))
+    tabItems(
+      tabItem(tabName = "dashboard"),
+      tabItem(tabName = "meet_the_squirrels"),
+      tabItem(tabName = "where_are_they"),
+      tabItem(tabName = "what_are_they_doing",
+              fluidRow(
+                box(plotOutput("squirrel_plot4", height = 250)), box(plotOutput("squirrel_plot2", height = 250))
+              )
+      ),
+      tabItem(tabName = "park_conditions")
     )
   )
 )
-
