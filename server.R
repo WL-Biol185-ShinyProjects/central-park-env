cb3f5d1d41a06ccab08133554f86924193081553
avagrace <aflory@mail.wlu.edu>
2026-03-09 16:18:33 -0400

    tail behavior visuals

--- server.R
+++ server.R
@@ -38,19 +38,19 @@ function(input, output) {
         axis.title = element_text(face = "bold")   # makes both axis labels bold
       )
   })
-  output$squirrel_plot5 <- renderPlot({ 
-    central_park_attitude %>%
-      filter(did_attitude == TRUE) %>%
-      count(attitude) %>%                                    # count per attitude
+  output$squirrel_plot3 <- renderPlot({ 
+    central_park_tailbeh_obs %>%
+      filter(did_tailbehavior == TRUE) %>%
+      count(tailbehavior) %>%                                    # count per attitude
       mutate(pct = n / sum(n),                               # calculate proportion
              label = scales::percent(pct, accuracy = 1)) %>% # e.g. "45%"
-      ggplot(aes(x = "", y = pct, fill = attitude)) +
+      ggplot(aes(x = "", y = pct, fill = tailbehavior)) +
       geom_bar(stat = "identity", width = 1) +
       coord_polar(theta = "y") +                             # this turns bar into pie
       geom_text(aes(label = label),
                 position = position_stack(vjust = 0.5),      # centers labels in slices
                 color = "white", fontface = "bold", size = 5) +
-      labs(title = "Pie Chart - Squirrel Attitude when Approached by Human", fill = "Attitude") +
+      labs(title = "Pie Chart - Squirrel Tail Behavior", fill = "Tail Behavior") +
       theme_void() +                                         # removes axes/gridlines
       theme(
         plot.title = element_text(hjust = 0.5, face = "bold", size = 14),
