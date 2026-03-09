4f1afd968016471cd4a153761c196d7b5489f7d9
jmoore <jmoorel@mail.wlu.edu>
2026-03-09 15:01:37 -0400

    adjusting formatting

--- ui.R
+++ ui.R
@@ -1,12 +1,7 @@
 library(shinydashboard)
 
-<<<<<<< HEAD
-dashboardPage((skin = "#652A0E"),
-=======
 dashboardPage(
->>>>>>> e4797d09b38855aea829ae3e7f3cddf36bb941e4
   dashboardHeader(title = "Squirrel Exploration"),
-  dashboardSidebar(title = "What's the squirrel doing?"),
   dashboardSidebar(
     sidebarMenu(
       menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
