a84fffd2733d242eac03c1dd539d4d7856fc6af1
Cindy <cxie@mail.wlu.edu>
2026-03-09 17:07:40 -0400

    fixed a bunch of things like adding graphs to all subtabs

--- server.R
+++ server.R
@@ -3,7 +3,25 @@ library(ggplot2)
 library(tidyverse)
 
 function(input, output) {
-  output$squirrel_plot1 <- renderPlot({ 
+  
+  output$squirrel_plot1 <- renderPlot({
+    central_park_attitude %>%
+      filter(did_attitude == TRUE) %>%
+      count(attitude) %>%
+      ggplot(aes(x = n, y = reorder(attitude, n))) +
+      geom_bar(stat = "identity", fill = "#652A0E", width = 0.5) +
+      geom_text(aes(label = scales::comma(n)), hjust = -0.2, fontface = "bold") +
+      scale_x_continuous(expand = expansion(mult = c(0, 0.15))) +
+      labs(title = "Bar Graph - Squirrel Attitude when Approached by Human", 
+           x = "Number of Squirrels", y = "Attitude") +
+      theme_minimal() +
+      theme(
+        plot.title = element_text(hjust = 0.5, face = "bold", size = 14),
+        axis.title = element_text(face = "bold")   # makes both axis labels bold
+      )
+  })
+  
+  output$squirrel_plot2 <- renderPlot({ 
     central_park_attitude %>%
       filter(did_attitude == TRUE) %>%
       count(attitude) %>%                                    # count per attitude
@@ -22,23 +40,25 @@ function(input, output) {
         legend.title = element_text(face = "bold")
       )
   })
-  output$squirrel_plot2 <- renderPlot({
-    central_park_attitude %>%
-      filter(did_attitude == TRUE) %>%
-      count(attitude) %>%
-      ggplot(aes(x = n, y = reorder(attitude, n))) +
+  
+  output$squirrel_plot3 <- renderPlot({
+    central_park_tailbeh_obs %>%
+      filter(did_tailbehavior == TRUE) %>%
+      count(tailbehavior) %>%
+      ggplot(aes(x = n, y = reorder(tailbehavior, n))) +
       geom_bar(stat = "identity", fill = "#652A0E", width = 0.5) +
       geom_text(aes(label = scales::comma(n)), hjust = -0.2, fontface = "bold") +
       scale_x_continuous(expand = expansion(mult = c(0, 0.15))) +
-      labs(title = "Bar Graph - Squirrel Attitude when Approached by Human", 
-           x = "Number of Squirrels", y = "Attitude") +
+      labs(title = "Bar Graph - Tail Behavior of Squirrel", 
+           x = "Number of Squirrels", y = "Tail Bevavior") +
       theme_minimal() +
       theme(
         plot.title = element_text(hjust = 0.5, face = "bold", size = 14),
         axis.title = element_text(face = "bold")   # makes both axis labels bold
       )
   })
-  output$squirrel_plot3 <- renderPlot({ 
+  
+  output$squirrel_plot4 <- renderPlot({ 
     central_park_tailbeh_obs %>%
       filter(did_tailbehavior == TRUE) %>%
       count(tailbehavior) %>%                                    # count per attitude
@@ -50,27 +70,11 @@ function(input, output) {
       geom_text(aes(label = label),
                 position = position_stack(vjust = 0.5),      # centers labels in slices
                 color = "white", fontface = "bold", size = 5) +
-      labs(title = "Pie Chart - Squirrel Tail Behavior", fill = "Tail Behavior") +
+      labs(title = "Pie Chart - Tail Behavior of Squirrel", fill = "Tail Behavior") +
       theme_void() +                                         # removes axes/gridlines
       theme(
         plot.title = element_text(hjust = 0.5, face = "bold", size = 14),
         legend.title = element_text(face = "bold")
-      )
-  })
-  output$squirrel_plot4 <- renderPlot({
-    central_park_tailbeh_obs %>%
-      filter(did_tailbehavior == TRUE) %>%
-      count(tailbehavior) %>%
-      ggplot(aes(x = n, y = reorder(tailbehavior, n))) +
-      geom_bar(stat = "identity", fill = "#652A0E", width = 0.5) +
-      geom_text(aes(label = scales::comma(n)), hjust = -0.2, fontface = "bold") +
-      scale_x_continuous(expand = expansion(mult = c(0, 0.15))) +
-      labs(title = "Bar Graph - Squirrel Tail Behavior", 
-           x = "Number of Squirrels", y = "Tail Bevavior") +
-      theme_minimal() +
-      theme(
-        plot.title = element_text(hjust = 0.5, face = "bold", size = 14),
-        axis.title = element_text(face = "bold")   # makes both axis labels bold
       )
   })
   
@@ -90,6 +94,64 @@ function(input, output) {
         axis.title = element_text(face = "bold") # makes both axis labels bold
       )
   })
+  
+  output$squirrel_plot6 <- renderPlot({
+    central_park_noise_obs %>%
+      filter(made_noise == TRUE) %>%
+      count(noise) %>%
+      mutate(pct = n / sum(n),                               # calculate proportion
+      label = scales::percent(pct, accuracy = 1)) %>% # e.g. "45%"
+      ggplot(aes(x = "", y = pct, fill = noise)) +
+      geom_bar(stat = "identity", width = 1) +
+      coord_polar(theta = "y") +                             # this turns bar into pie
+      geom_text(aes(label = label),
+              position = position_stack(vjust = 0.5),      # centers labels in slices
+              color = "white", fontface = "bold", size = 5) +
+    labs(title = "Pie Chart - Squirrel Noises", fill = "Noises") +
+    theme_void() +                                         # removes axes/gridlines
+    theme(
+      plot.title = element_text(hjust = 0.5, face = "bold", size = 14),
+      legend.title = element_text(face = "bold")
+    )
+})
+  
+  output$squirrel_plot7 <- renderPlot({
+    central_park_act_obs %>%
+      filter(did_activity == TRUE) %>%
+      count(activity) %>%
+      ggplot(aes(x = n, y = reorder(activity, n))) +
+      geom_bar(stat = "identity", fill = "#652A0E", width = 0.5) +
+      geom_text(aes(label = scales::comma(n)), hjust = -0.2, fontface = "bold") +
+      scale_x_continuous(expand = expansion(mult = c(0, 0.15))) +
+      labs(title = "Bar Graph - Squirrel Activity", 
+           x = "Number of Squirrels", y = "Activity") +
+      theme_minimal() +
+      theme(
+        plot.title = element_text(hjust = 0.5, face = "bold", size = 14),
+        axis.title = element_text(face = "bold") # makes both axis labels bold
+      )
+  })
+  
+  output$squirrel_plot8 <- renderPlot({
+    central_park_act_obs %>%
+      filter(did_activity == TRUE) %>%
+      count(activity) %>%
+      mutate(pct = n / sum(n),                               # calculate proportion
+             label = scales::percent(pct, accuracy = 1)) %>% # e.g. "45%"
+      ggplot(aes(x = "", y = pct, fill = activity)) +
+      geom_bar(stat = "identity", width = 1) +
+      coord_polar(theta = "y") +                             # this turns bar into pie
+      geom_text(aes(label = label),
+                position = position_stack(vjust = 0.5),      # centers labels in slices
+                color = "white", fontface = "bold", size = 5) +
+      labs(title = "Pie Chart - Squirrel Activity", fill = "Activity") +
+      theme_void() +                                         # removes axes/gridlines
+      theme(
+        plot.title = element_text(hjust = 0.5, face = "bold", size = 14),
+        legend.title = element_text(face = "bold")
+      )
+  })  
+
 }
 
 
--- ui.R
+++ ui.R
@@ -8,9 +8,9 @@ dashboardPage(skin = "green",
   dashboardSidebar(title = "Squirrels in Central Park",
     sidebarMenu(
       menuItem("Dashboard", tabName = "dashboard", icon = icon("dashboard")),
-      menuItem("Meet the Squirrels", tabName = "meet_the_squirrels", icon = icon("tree")),
-      menuItem("Where Are They", tabName = "where_are_they", icon = icon("map")),
-      menuItem("What are They Doing", icon = icon("walking"), 
+      menuItem("Meet the Squirrels!", tabName = "meet_the_squirrels", icon = icon("tree")),
+      menuItem("Where Are They?", tabName = "where_are_they", icon = icon("map")),
+      menuItem("What are They Doing?", icon = icon("walking"), 
           menuSubItem("Attitude", tabName = "Attitude"),
           menuSubItem("Activity", tabName = "Activity"),
           menuSubItem("Tail Behavior", tabName = "Tail_Behavior"),
@@ -26,7 +26,7 @@ dashboardPage(skin = "green",
       tabItem(tabName = "where_are_they"),
       tabItem(tabName = "Attitude",
               fluidRow(
-                box(plotOutput("squirrel_plot1", height = 250)), box(plotOutput("squirrel_plot2", height = 250))
+                box(plotOutput("squirrel_plot1")), box(plotOutput("squirrel_plot2"))
               )),
       tabItem(tabName = "Tail_Behavior",
               fluidRow(
@@ -36,6 +36,10 @@ dashboardPage(skin = "green",
                fluidRow(
                  box(plotOutput("squirrel_plot5")), box(plotOutput("squirrel_plot6"))
               )),    
+      tabItem(tabName = "Activity",
+              fluidRow(
+                box(plotOutput("squirrel_plot7")), box(plotOutput("squirrel_plot8"))
+              )),
       tabItem(tabName = "park_conditions")
       )
     )
