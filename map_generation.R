8b43f3b9f11cd732522e61b55452da070b79511f
Cindy <cxie@mail.wlu.edu>
2026-03-16 16:47:46 -0400

    edits to maps

--- map_generation.R
+++ map_generation.R
@@ -10,15 +10,15 @@ leaflet(data = central_park)%>%
   addCircleMarkers(
     lng = ~X,
     lat = ~Y,
-    radius = 2,
-    color = "brown",
+    radius = 2.5,
+    color = "#8B4513",
     fillOpacity = 0.7,
     stroke = FALSE
   )
 
 # HEAT MAP
 leaflet(data = central_park) %>%
-  setView(lng = -73.9683, lat = 40.7851, zoom = 20) %>%
+  setView(lng = -73.9683, lat = 40.7851, zoom = 14) %>%
   addProviderTiles(providers$Esri.NatGeoWorldMap) %>%  
   addHeatmap(
     lng = ~X,
@@ -26,7 +26,17 @@ leaflet(data = central_park) %>%
     blur = 25,      # how smooth/spread out the heat is
     max = 0.05,     # intensity ceiling
     radius = 17    # size of each point's influence
-  ) # blue is low density, green/yellow is med density, red is high density
+  ) %>%
+  addControl(
+    html = '
+      <div style="background:white; padding:8px; border-radius:5px;">
+        <b>Squirrel Density</b><br>
+        <span style="color:#0000FF;">&#9632;</span> Low<br>
+        <span style="color:#00FF00;">&#9632;</span> Medium<br>
+        <span style="color:#FF0000;">&#9632;</span> High
+      </div>',
+    position = "bottomright"
+  )
 
 
 # creating a new temp table where we look at AGE (removing "NA" and "?" first)
@@ -48,7 +58,7 @@ leaflet(data = age)%>%
   addCircleMarkers(
     lng = ~X,
     lat = ~Y,
-    radius = 2,
+    radius = 3,
     color = ~pal(Age),
     fillOpacity = 0.7,
     stroke = FALSE
@@ -78,7 +88,7 @@ leaflet(data = fur)%>%
   addCircleMarkers(
     lng = ~X,
     lat = ~Y,
-    radius = 2,
+    radius = 3,
     color = ~pal(fur_color),
     fillOpacity = 0.7,
     stroke = FALSE
@@ -108,7 +118,7 @@ leaflet(data = litter)%>%
   addCircleMarkers(
     lng = ~X,
     lat = ~Y,
-    radius = 2,
+    radius = 3,
     color = ~pal(Litter),
     fillOpacity = 0.7,
     stroke = FALSE
@@ -122,7 +132,34 @@ leaflet(data = litter)%>%
 
 # LITTER MAP 2
 
+litter <- litter %>% mutate(litter_size = case_when(
+  Litter == "None" ~ 2,
+  Litter == "Some" ~ 5,
+  Litter == "Abundant" ~ 9
+))
 
+pal <- colorFactor(
+  palette = c("#00CC00", "#FFD700", "#FF0000"),
+  levels = c("None", "Some", "Abundant")
+)
+
+leaflet(data = litter) %>% 
+  setView(lng = -73.9683, lat = 40.7851, zoom = 14) %>% 
+  addProviderTiles(providers$Esri.NatGeoWorldMap) %>%
+  addCircleMarkers(
+    lng = ~X,
+    lat = ~Y,
+    radius = ~litter_size,
+    color = ~pal(Litter),
+    fillOpacity = 0.7,
+    stroke = FALSE
+  ) %>%
+  addLegend(
+    position = "bottomright",
+    pal = pal,
+    values = ~Litter,
+    title = "Litter Amount"
+  )
 
 
 
