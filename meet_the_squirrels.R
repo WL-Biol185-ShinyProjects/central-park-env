7bcdbedf6a838e9805b98ef73581f6e084e5d7fe
jmoore <jmoorel@mail.wlu.edu>
2026-03-16 15:21:03 -0400

    creating meet the squirrels text box

--- meet_the_squirrels.R
+++ meet_the_squirrels.R
@@ -2,4 +2,9 @@ library(shiny)
 
 meet_the_squirrels <- tabItem(tabName = "meet_the_squirrels",
                               #insert code here
-)
+                  fluidRow(
+                    box(
+                      "About the Squirrels"
+                    )
+                  )
+                              )
