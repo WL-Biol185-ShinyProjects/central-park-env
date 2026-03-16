70b04e990b70699510769124be7c80f3d4b5f9eb
jmoore <jmoorel@mail.wlu.edu>
2026-03-16 16:48:19 -0400

    about the squirrels intro

--- meet_the_squirrels.R
+++ meet_the_squirrels.R
@@ -2,11 +2,26 @@ library(shiny)
 
 meet_the_squirrels <- tabItem(tabName = "meet_the_squirrels",
                               #insert code here
+
                   fluidRow(
                     box(
-                      "About the Squirrels", title = "About the Squirrels", width = 12
-                    )
-                  )
-                              )
+                      width = 12,
+                      div(
+                        style = "width: 100%; height: 400px; overflow: hidden;",
+                        tags$img(
+                          src = "https://media.istockphoto.com/id/1667398983/photo/squirrel-in-central-park-new-york-city.jpg?s=612x612&w=0&k=20&c=uz0a7GzAq5CbfBlEQcAelOqtRig0pHmrpUBsWHTifRE=",
+                          style = "width: 100%; height: 100%; object-fit: contain;"
+                        )
+                      )
+                    ),
+                  fluidRow(
+                    box(
+                      p("This app focuses on an open access data set compiled by the 2018 squirrel census available through NYC open data.  This data set was collected through 323 volunteer squirrel sighters in October of 2018 and findings regarding the population size of a particular grey squirrel species (sciurus carolinensis) within Central Park. However, there is so much more to the Central Park squirrel community than just the number of eastern grey squirrels present, and it is those dynamics that you can see here!"), title = "About the Squirrels", width = 12
+                      )
+                          )
+                       
+                          
+                          )
+)
 
 
