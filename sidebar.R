ac6287f3570747e7701e9f2cb90f436694511ecd
Cindy <cxie@mail.wlu.edu>
2026-03-18 16:33:11 -0400

    changed capital

--- sidebar.R
+++ sidebar.R
@@ -4,7 +4,7 @@ sidebar <- dashboardSidebar(title = "Squirrels in Central Park",
                 sidebarMenu(
                       menuItem("Meet the Squirrels", tabName = "meet_the_squirrels", icon = icon("tree")),
                       menuItem("Where Are They", tabName = "where_are_they", icon = icon("map")),
-                      menuItem("What are They Doing", icon = icon("walking"),
+                      menuItem("What Are They Doing", icon = icon("walking"),
                           menuSubItem("Attitude", tabName = "Attitude"),
                           menuSubItem("Activity", tabName = "Activity"),
                           menuSubItem("Tail Behavior", tabName = "Tail_Behavior"),
