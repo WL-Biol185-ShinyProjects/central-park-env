1bb01a1ead8209cf238b289758fc1f25cae93f57
Cindy <cxie@mail.wlu.edu>
2026-03-23 16:45:40 -0400

    changed a line

--- server.R
+++ server.R
@@ -10,9 +10,6 @@ source("park_conditions_data_organization_file.R")
 source("Compiled_Observation_Tables.R")
 source("map_generation.R")
 
-central_park <- read_csv("central_park_og.csv")
-
-
 # DATA PREP FOR MAP GENERATION
 
 # age map
