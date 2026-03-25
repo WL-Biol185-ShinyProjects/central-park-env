686211872ccfa5d1c6ae0972cc0880edc0a7d5ec
Emma Batty <ebatty@mail.wlu.edu>
2026-03-25 16:35:43 -0400

    fixed webpage data loading

--- Compiled_Observation_Tables.R
+++ Compiled_Observation_Tables.R
@@ -2,9 +2,9 @@ library(tidyverse)
 library(dplyr) # joining the datasets together
 library(purrr)
 
-squirrel_data <- read_csv("~/central-park-env/raw_data/2018_Central_Park_Squirrel_Census_-_Squirrel_Data_20260216.csv")
-hectare_data <- read_csv("~/central-park-env/raw_data/2018_Central_Park_Squirrel_Census_-_Hectare_Data_20260216.csv")
-story_data <- read_csv("~/central-park-env/raw_data/2018_Central_Park_Squirrel_Census_-_Stories_20260216 (1).csv")
+squirrel_data <- read_csv("raw_data/2018_Central_Park_Squirrel_Census_-_Squirrel_Data_20260216.csv")
+hectare_data <- read_csv("raw_data/2018_Central_Park_Squirrel_Census_-_Hectare_Data_20260216.csv")
+story_data <- read_csv("raw_data/2018_Central_Park_Squirrel_Census_-_Stories_20260216 (1).csv")
 
 df_list <- list(squirrel_data, hectare_data, story_data) # add all your dataframes to a list
 
