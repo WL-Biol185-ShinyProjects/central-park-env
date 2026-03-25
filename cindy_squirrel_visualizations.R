5d0dcb9a14fa958d0422297e343108e859e237fc
Cindy <cxie@mail.wlu.edu>
2026-03-25 16:49:31 -0400

    made a new graph

--- cindy_squirrel_visualizations.R
+++ cindy_squirrel_visualizations.R
@@ -24,11 +24,27 @@ ggplot(total_daily_sighters, aes(x = Date, y = total_daily_sighters)) +
   theme_minimal() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-# bar graph breakdown by AM PM - stacked
+# bar graph breakdown by AM PM - stacked bars
 
-sighters <- data_table %>%
+sighters_by_time <- data_table %>%
   select(Date, Shift, Number.of.sighters) %>%
-  mutate(Date = as.character(Date),
-         Date = as.Date(Date, format = "%m%d%Y")) %>%
+  mutate(Date = as.Date(as.character(Date), format = "%m%d%Y")) %>%
   group_by(Date, Shift) %>%
-  summarise(Total_Sighters = sum(Number.of.sighters, na.rm = TRUE), .groups = "drop")
+  summarise(Number.of.sighters = sum(Number.of.sighters, na.rm = TRUE), .groups = "drop")
+
+ggplot(sighters_by_time, aes(x = Date, y = Number.of.sighters, fill = Shift)) +
+  geom_bar(stat = "identity", position = "stack") +
+  scale_fill_manual(values = c("AM" = "#5BA08A", "PM" = "#B87333")) +
+  labs(
+    title = "Total Number of Sighters per Day - Breakdown by AM vs PM",
+    x = "Date",
+    y = "Number of Sighters",
+    fill = "Time of Day"
+  ) +
+  scale_x_date(
+    breaks = unique(sighters_by_time$Date),
+    date_labels = "%b %d"
+  ) +
+  scale_y_continuous(breaks = seq(0, max(sighters_by_time$Number.of.sighters) + 150, by = 50)) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
