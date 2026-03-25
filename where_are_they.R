6de717b6b844c87b0c8d3cc504f3f7ae4131dbb1
Cindy <cxie@mail.wlu.edu>
2026-03-25 15:15:55 -0400

    trying to resolve issue

--- where_are_they.R
+++ where_are_they.R
@@ -3,22 +3,16 @@ library (shiny)
 where_are_they <- tabItem(tabName = "where_are_they",
                           fluidRow(
                             box(
-                              width = 12,
+                              width = 3,
+                              height = "600px",
                               title = "Squirrels in Central Park",
                               selectInput("map_choice",
                                           label = "Select Map",
                                           choices = c("Squirrel Density", "Squirrel Heat Map", "Human Density", "By Fur Color", "By Litter Amount", "By Age"))
-                            )
-                          ),
-                          fluidRow(
+                            ),
                             box(
-                              width = 12,
-                              height = "800px",
-                              tags$head(tags$style(HTML("
-      .box-body {
-        padding-top: 40px !important;
-      }
-    "))),
+                              width = 9,
+                              height = "600px",
                               leafletOutput("squirrel_map", height = "580px")
                             )
                           )
